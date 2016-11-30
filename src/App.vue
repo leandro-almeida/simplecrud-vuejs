ae4cf2e5b4466fd2f1987d6626104d45bbdb3bb9
leandroalmeida <almeida85@gmail.com>
2016-11-30 17:31:03 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Cervejarias></Cervejarias>
+  </div>
+</template>
+
+<script>
+  import Cervejarias from './Cervejarias.vue'
+
+  export default {
+    components: {
+      "Cervejarias": Cervejarias
+    }
+  }
+</script>
+
+<style>
+  body {
+    font-family: Helvetica, sans-serif;
+  }
+</style>
